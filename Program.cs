24b47f89d097d34b46fc12746616af556bcf427b
Inarixdono <jenielwtf@gmail.com>
2023-11-02 16:05:16 -0400

    Acabé la semana del 30 de octubre al 5 de noviembre. Se agregan los métodos `EjemploIf()`, `EjemploFor()`, `EjemploForEach()`, `EjemploWhile()`, `EjemploTryCatch()`.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿namespace Pitagoras
-{ 
+﻿using System;
+
+namespace Pitagoras
+{
+
     class Program
     {
         class Persona
@@ -49,11 +52,24 @@
             }
         }
 
+        static string AddWhiteSpace(string cadena, char caracter, int whiteSpaces)
+        {
+            int index = cadena.IndexOf(caracter);
+            string newString = cadena.Insert(index, new string(' ', whiteSpaces));
+            return newString;
+        }
+
+        static void WaitForUser()
+        {
+            Console.WriteLine("\nPresione cualquier tecla para continuar...");
+            Console.ReadKey(false);
+            Console.Clear();
+        }
+
         static void HelloWord()
         {
             Console.WriteLine("Hello, World!");
-            Console.ReadKey(false);
-            Console.Clear();
+            WaitForUser();
         }
 
         static void Saludar()
@@ -67,8 +83,7 @@
             }
 
             Console.WriteLine($"Hola, {jeniel.nombre}, tienes {jeniel.edad} años de edad.");            
-            Console.ReadKey(false);
-            Console.Clear();
+            WaitForUser();
         }
 
         static void Operadores(int valor1, int valor2)
@@ -116,22 +131,141 @@
             Console.WriteLine($"true or false: {true || false}");
             Console.WriteLine($"not true: {!true}");
             Console.WriteLine($"true ir false: {true ^ false}");
-            Console.ReadKey(false);
-            Console.Clear();
+            WaitForUser();
         }
 
+        static void EjemploIf()
+        {
+            int edad = 0;
+            Console.Write("Ingresa tu edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Eres mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Eres menor de edad");
+            }
+
+            WaitForUser();
+        }
+
+        static void EjemploFor()
+        {
+            Console.Write("Ingresa el número a mostrar su tabla de multiplicación: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 13; i++)
+            {
+                string output = $"{num} x {i} = {num * i}";
+                if (i < 10)
+                {
+                    Console.WriteLine(AddWhiteSpace(output, '=', 1));
+                }
+                else
+                {
+                    Console.WriteLine(output);
+                }
+            }
+
+            WaitForUser();
+        }
+
+        static void EjemploForEach()
+        {
+            Persona jeniel = new Persona("Jeniel", 22);
+            Persona dioselina = new Persona("Dioselina", 20);
+            Persona gadiel = new Persona("Gadiel", 21);
+            Persona carmen = new Persona("Carmen", 40);
+            Persona rachel = new Persona("Rachel", 20);
+            Persona joseph = new Persona("Joseph", 25);
+            Persona[] personas = { jeniel, dioselina, gadiel, carmen, rachel, joseph };
+
+            int longitudNombre = personas.Max(persona => persona.nombre.Length);
+            int longitudSeparador = longitudNombre + 12;
+            string separador = new string('-', longitudSeparador);
+            string[] header = { "Nombre", "Edad" };
+
+            Console.WriteLine("Personas existentes:\n");
+            Console.WriteLine(separador);
+            Console.WriteLine($"| {header[0].PadRight(longitudNombre)}  | {header[1].PadRight(3)} |");
+
+            foreach (Persona persona in personas)
+            {
+                string nombre = persona.nombre.PadRight(longitudNombre);
+                string edad = persona.edad.ToString().PadRight(3);
+                string output = $"| {nombre}  |  {edad} |";
+                Console.WriteLine(separador);
+                Console.WriteLine(output);
+            }
+
+            Console.WriteLine(separador);
+            WaitForUser();
+        }
+
+        static void EjemploWhile()
+        {
+            while (true)
+            {
+                Console.WriteLine("Este bucle solo terminará cuando se presione la tecla \"a\".");
+                Console.Write("Presione cualquier tecla para continuar...: ");
+                string key = Console.ReadLine();
+                if (key.ToLower() == "a")
+                {
+                    break;
+                }
+                Console.Clear();
+            }
+
+            Console.WriteLine("Bucle terminado.");
+            WaitForUser();
+        }
+
+        static void EjemploTryCatch()
+        {
+            int num1, num2;
+
+            Console.WriteLine("Dividir dos números...\n");
+            Console.Write("Ingresa el primer número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingresa el segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("No se puede dividir entre 0");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("De entrada solo se admiten números.");
+            }
+
+            WaitForUser();
+        }
         static void Menu()
         {
             int opcion;
             String menu = "1. Hello, World!\n" +
                           "2. Saludar\n" +
                           "3. Operadores\n" +
+                          "4. Ejemplo If\n" +
+                          "5. Ejemplo For\n" +
+                          "6. Ejemplo ForEach\n" +
+                          "7. Ejemplo While\n" +
+                          "8. Ejemplo Try Catch\n" +
                           "0. Salir\n";
 
             do 
             {
                 Console.WriteLine(menu);
                 Console.Write("Elija una opción: ");
+
                 opcion = int.Parse(Console.ReadLine());
                 Console.Clear();
                 switch (opcion)
@@ -148,8 +282,34 @@
                         Operadores(10, 5);
                         break;
 
+                    case 4:
+                        EjemploIf();
+                        break;
+
+                    case 5:
+                        EjemploFor();
+                        break;
+                    
+                    case 6:
+                        EjemploForEach();
+                        break;
+
+                    case 7:
+                        EjemploWhile();
+                        break;
+
+                    case 8:
+                        EjemploTryCatch();
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Saliendo del programa...");
+                        WaitForUser();
+                        break;
+
                     default:
                         Console.WriteLine("Opción no válida");
+                        WaitForUser();
                         break;
                 }
             }
